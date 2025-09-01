b7f2c2d2999478515b214ebe710926e1a51ac379
Samuel Thompson <samthompson1818@gmail.com>
2025-09-01 13:35:15 -0400

    Create boat.js + initial calculation function

---
+++ tools/boat.js
@@ -0,0 +1,17 @@
+function submit() {
+  const gasVolumeInput = document.getElementById("gasVolume").value;
+  const gasPriceInput = document.getElementById("gasPrice").value;
+  const milesDrivenInput = document.getElementById("milesDriven").value;
+
+  const response = document.getElementById("gasInfo");
+
+  gasMileage = milesDrivenInput / gasVolumeInput;
+  costPerMile = gasPriceInput / milesDrivenInput;
+
+  response.innerHTML = `Fuel Economy for the last ${milesDrivenInput} miles: ${gasMileage}mi/G \n
+                        Gas Cost per Mile for the last ${milesDrivenInput} miles: $${costPerMile}`;
+  
+}
+
+const button = document.getElementById("submitButton");
+button.addEventListener('click', sumbit);
