ad9b9f1eb4f58a543ae5baa81d82726100b9e16e
Jamboxman5 <samthompson1818@gmail.com>
2025-09-14 22:54:56 -0400

    Added deployment to pages for react app

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { AuthProvider } from "./services/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </HashRouter>
   </StrictMode>
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,8 +12,16 @@ export default function HomePage() {
         <h1 className="text-center font-bold text-white">
           Welcome to my Portfolio!
         </h1>
+        <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center">
+          <h1 className="text-3xl font-bold text-gray-900 mb-4">My Stuff:</h1>
 
-        <div className="pt-16"></div>
+          <a
+            href="/tools/boat.html"
+            className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition"
+          >
+            Boat Fuel Economy Calculator
+          </a>
+        </div>{" "}
       </div>
     </div>
   );
