aaa3c45f491d7b618fdba6678e2fd1f9e99fb078
Jamboxman5 <sthomps9@oswego.edu>
2025-09-17 01:52:49 -0400

    Redesigned navbar + moved games to designated component

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import GamesList from "./GamesList";
 
 export default function NavigationBar() {
   const navigate = useNavigate();
@@ -8,37 +9,28 @@ export default function NavigationBar() {
   };
 
   return (
-    <nav className="bg-gradient-to-l from-blue-500 to-indigo-700 text-white shadow-md fixed top-0 left-0 w-full z-50 max-h-[25vh] overflow-y-auto">
+<nav className="bg-gradient-to-r from-gray-900 to-indigo-900 text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-8xl mx-auto px-4">
         <ul className="flex space-x-7 py-5">
           <button
             onClick={handleHome}
-            className="!bg-transparent hover:!bg-indigo-600 text-white font-semibold py-2 px-4 rounded transition duration-200"
+            className="!bg-transparent hover:!bg-gray-800 text-white font-semibold py-2 px-4 rounded transition duration-200"
           >
             Projects
           </button>
           <button
             onClick={handleHome}
-            className="!bg-transparent hover:!bg-indigo-600 text-white font-semibold py-2 px-4 rounded transition duration-200"
+            className="!bg-transparent hover:!bg-gray-800 text-white font-semibold py-2 px-4 rounded transition duration-200"
           >
             Tools
           </button>
-          <button
-            onClick={handleHome}
-            className="!bg-transparent hover:!bg-indigo-600 text-white font-semibold py-2 px-4 rounded transition duration-200"
-          >
-            Games
-          </button>
-          <button
-            onClick={handleHome}
-            className="!bg-transparent hover:!bg-indigo-600 text-white font-semibold py-2 px-4 rounded transition duration-200"
-          >
-            Art
-          </button>
+          
+          <GamesList/>
+
 
           <button
             onClick={handleHome}
-            className="ml-auto !bg-transparent hover:!bg-blue-400 text-white font-semibold py-2 px-4 rounded transition duration-200"
+            className="ml-auto !bg-transparent hover:!bg-indigo-800 text-white font-semibold py-2 px-4 rounded transition duration-200"
           >
             Home
           </button>
