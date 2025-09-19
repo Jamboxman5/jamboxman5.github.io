ef0c12120ce437c1468021a2d1a440871e0c56f3
Jamboxman5 <sthomps9@oswego.edu>
2025-09-18 22:34:18 -0400

    Added projects drop down to navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import GamesList from "./GamesList";
 import ToolsList from "./ToolsList";
+import ProjectsList from "./ProjectsList";
 
 export default function NavigationBar() {
   const navigate = useNavigate();
@@ -13,12 +14,7 @@ export default function NavigationBar() {
 <nav className="bg-gradient-to-r from-gray-900 to-indigo-900 text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-8xl mx-auto px-4">
         <ul className="flex space-x-7 py-5">
-          <button
-            onClick={handleHome}
-              className="!bg-transparent hover:!bg-gray-800 text-white !text-lg font-semibold py-2 px-4 rounded transition duration-200 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center"
-          >
-            Projects
-          </button>
+          <ProjectsList/>
           <ToolsList/>
           
           <GamesList/>
