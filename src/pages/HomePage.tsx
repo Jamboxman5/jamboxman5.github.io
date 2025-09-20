c540390e36280776ab005bf7376afdb27b2d342f
Jamboxman5 <samthompson1818@gmail.com>
2025-09-19 23:48:16 -0400

    Redesigned homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,32 +9,31 @@ export default function HomePage() {
     <div className="min-h-screen">
       <div className="w-screen bg-gradient-to-br from-indigo-800 to-orange-800 pt-40 pb-40 min-h-screen">
         <NavigationBar />
-        <h1 className="text-center font-bold text-white">
-          Welcome to my Portfolio!
-        </h1>
-        <div className="mx-auto my-20 py-10 bg-gray-900 shadow-lg rounded-2xl p-8 max-w-md w-full text-center">
-          <p className="text-3xl font-plain text-gray-300 mb-4">My Stuff:</p>
+        <h1 className="text-center font-bold text-white">Samuel Thompson</h1>
+        <h2 className="text-center font-semibold text-gray-200 mt-6">
+          B.S. Software Engineering
+        </h2>
+        <div className="mx-auto my-10 py-10 bg-gray-800 shadow-lg rounded-2xl p-8 max-w-md w-full text-center">
+          <p className="text-3xl font-semibold text-gray-100 mb-4">
+            Live Projects:
+          </p>
 
-          <div className="flex flex-col space-y-10">
-
-          <button
-            onClick={() => (window.location.href = "/tools/boat.html")}
-            className="mt-5 bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-200"
-          >
-            Fuel Economy Calculator
-          </button>
-          <button
-            onClick={() =>
-              (window.location.href = "/CSC495-AccommoDate-Interface/")
-            }
-            className="mt-5 bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-200"
-          >
-            AccommoDate
-          </button>
-
+          <div className="flex flex-col space-y-4">
+            <button
+              onClick={() => (window.location.href = "/tools/boat.html")}
+              className="mt-5 bg-gradient-to-t from-purple-800 to-indigo-800 hover:!bg-blue-600 text-white font-medium py-3 px-4 rounded-lg transition-colors duration-200"
+            >
+              Fuel Economy Calculator
+            </button>
+            <button
+              onClick={() =>
+                (window.location.href = "/CSC495-AccommoDate-Interface/")
+              }
+              className="mt-5 bg-gradient-to-t from-purple-800 to-indigo-800 hover:!bg-blue-600 text-white font-medium py-3 px-4 rounded-lg transition-colors duration-200"
+            >
+              AccommoDate
+            </button>
           </div>
-
-          
         </div>
       </div>
     </div>
