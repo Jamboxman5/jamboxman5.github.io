9f1585e38c589ff45011f166b8979c6f636bd22a
Jamboxman5 <samthompson1818@gmail.com>
2025-09-24 12:00:00 -0400

    Inverted navbar components

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -14,18 +14,23 @@ export default function NavigationBar() {
 <nav className="bg-gradient-to-r from-gray-900 to-indigo-900 text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-8xl mx-auto px-4">
         <ul className="flex space-x-7 py-5">
-          <ProjectsList/>
-          <ToolsList/>
-          
-          <GamesList/>
-
-
           <button
             onClick={handleHome}
-            className="ml-auto !bg-transparent hover:!bg-indigo-800 text-white font-semibold py-2 px-4 rounded transition duration-200"
+            className="!bg-transparent hover:!bg-gray-800 text-white font-semibold py-2 px-4 rounded transition duration-200"
           >
             Home
           </button>
+          
+          <div className="flex space-x-7 ml-auto">
+<ProjectsList/>
+          <ToolsList/>
+          
+          <GamesList/>
+          </div>
+          
+
+
+          
         </ul>
       </div>
     </nav>
