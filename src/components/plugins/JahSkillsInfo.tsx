7946fa243313750ad8ce66c432327bf07464801f
Jamboxman5 <samthompson1818@gmail.com>
2025-09-27 00:29:09 -0400

    Created distinct components for each spigot plugin

---
+++ src/components/plugins/JahSkillsInfo.tsx
@@ -0,0 +1,44 @@
+export default function JahSkillsInfo() {
+    return (
+        <div className="mx-auto my-10 py-10 bg-gray-800 shadow-lg rounded-4xl p-8 w-8/10 lg:max-w-7/16 lg:max-w-7/16 p-4 text-center">
+            
+            <p className="text-3xl font-bold text-gray-100 mb-4">
+              - JahSkills -
+            </p>
+
+            <p className="text-xl font-semibold text-gray-300 w-5/6 mx-auto">
+              This plugin is meant to add RPG style skills & perks to the game. 
+              It adds 8 different skill fields, each with 9 different unlockable perks.
+              It also adds an experience, leveling, and skill point system.
+            </p>
+
+            <div className="flex flex-row w-3/4 mx-auto my-6">
+
+              <button
+                onClick={() =>
+                  (window.location.href =
+                    "https://github.com/Jamboxman5/JahSkills")
+                }
+                className="mx-auto mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium text-lg py-4 w-7/16 rounded-2xl transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
+              >
+                Source Code
+              </button>
+              <button
+                onClick={() =>
+                  (window.location.href =
+                    "https://github.com/Jamboxman5/JahSkills")
+                }
+                className="mx-auto mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium text-lg py-4 w-7/16 rounded-2xl transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
+              >
+                SpigotMC Page
+              </button>
+
+            </div>
+
+            <p className="text-xl font-semibold text-gray-400 text-left">
+              Dependencies: Vault <br/> Soft-Dependencies: PlaceholderAPI 
+            </p>
+
+          </div>
+    )
+}
--- src/pages/SpigotPage.tsx
+++ src/pages/SpigotPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import NavigationBar from "../components/NavigationBar";
+import JahSkillsInfo from "../components/plugins/JahSkillsInfo";
 
 export default function SpigotPage() {
   useEffect(() => {
@@ -9,49 +10,15 @@ export default function SpigotPage() {
       <div className="bg-gradient-to-br from-indigo-800 to-orange-800 pt-40 pb-40 min-h-screen">
         <NavigationBar />
         <h1 className="text-center font-bold text-white">Spigot Plugins</h1>
-        <h2 className="text-center font-semibold text-gray-200 mt-6">
+        <p className="text-center font-semibold text-xl text-gray-200 w-8/16 mx-auto mt-6">
           Minecraft multiplayer server modifications, implemented using the
           Spigot API, Vault API, and various other dependencies.
-        </h2>
+        </p>
 
         <div className="flex flex-wrap space-x-10 space-y-10">
           
         
-          <div className="mx-auto my-10 py-10 bg-gray-800 shadow-lg rounded-4xl p-8 w-7/16 p-4 text-center">
-            <p className="text-3xl font-semibold text-gray-100 mb-4">
-              Plugin Sources:
-            </p>
-
-            <div className="flex flex-col space-y-4">
-              <button
-                onClick={() =>
-                  (window.location.href =
-                    "https://github.com/Jamboxman5/JahSkills")
-                }
-                className="mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
-              >
-                JahSkills
-              </button>
-              <button
-                onClick={() =>
-                  (window.location.href =
-                    "https://github.com/Jamboxman5/WesternHunting")
-                }
-                className="mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
-              >
-                Western Hunting
-              </button>
-              <button
-                onClick={() =>
-                  (window.location.href =
-                    "https://github.com/Jamboxman5/Western-Bounties")
-                }
-                className="mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
-              >
-                Western Bounties
-              </button>
-            </div>
-          </div>
+          <JahSkillsInfo/>
           
         </div>
       </div>
