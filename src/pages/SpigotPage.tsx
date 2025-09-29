9e3d588ae874c6d34820ad672e7a1b8f09c1de17
Jamboxman5 <samthompson1818@gmail.com>
2025-09-28 23:18:22 -0400

    Added spigot logo to plugins page

--- src/pages/SpigotPage.tsx
+++ src/pages/SpigotPage.tsx
@@ -4,6 +4,7 @@ import JahSkills from "../components/plugins/JahSkills";
 import WesternBounties from "../components/plugins/WesternBounties";
 import WesternHunting from "../components/plugins/WesternHunting";
 import SixPocketShop from "../components/plugins/SixPocketShop";
+import spigotPng from '/spigot.png'; // Relative path from your component
 
 export default function SpigotPage() {
   useEffect(() => {
@@ -12,12 +13,15 @@ export default function SpigotPage() {
   return (
       <div className="bg-gradient-to-br from-indigo-800 to-orange-800 pt-40 pb-40 min-h-screen">
         <NavigationBar />
-        <h1 className="text-center font-bold text-white">Spigot Plugins</h1>
-        <p className="text-center font-semibold text-xl text-gray-200 w-8/16 mx-auto mt-6">
-          A collection of my Minecraft multiplayer server modifications, implemented using the
-          Spigot API, Vault API, and various other dependencies. Some of these were developed with public use in mind,
-          others specifically to be used by my own server, and are tailored as such.
-        </p>
+        <div className="flex flex-col space-y-4">
+          <img src={spigotPng} className="mx-auto"/>
+          <h1 className="text-center font-bold text-white">Spigot Plugins</h1>
+          <p className="text-center font-semibold text-xl text-gray-200 w-8/16 mx-auto mt-6">
+            A collection of my Minecraft multiplayer server modifications, implemented using the
+            Spigot API, Vault API, and various other dependencies. Some of these were developed with public use in mind,
+            others specifically to be used by my own server, and are tailored as such.
+          </p>
+        </div>
 
         <div className="flex flex-wrap space-x-10 space-y-10 my-10">
           
