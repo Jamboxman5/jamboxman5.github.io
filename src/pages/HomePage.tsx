174b046c833a76909507761bdbeaaaf654b83a9e
Jamboxman5 <samthompson1818@gmail.com>
2025-10-10 12:00:00 -0400

    Vertically centered live projects section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -149,22 +149,23 @@ export default function HomePage() {
           </div>
 
           <div className="mx-auto my-10 py-10 bg-gray-800 shadow-lg rounded-4xl p-8 w-8/10 lg:max-w-7/16 text-center">
-            <div className="flex flex-col space-y-4">
+            <div className="flex flex-col items-center justify-center space-y-4 h-full">
               <p className="text-white text-3xl text-center font-bold">
                 Live Projects:
               </p>
 
               <button
                 onClick={() => navigate("/reacttools/fueleco")}
-                className="mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
+                className=" w-full mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
               >
                 Fuel Economy Calculator
               </button>
+
               <button
                 onClick={() =>
                   (window.location.href = "/CSC495-AccommoDate-Interface/#/")
                 }
-                className="mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
+                className=" w-full mt-5 bg-gradient-to-t from-indigo-800 to-indigo-800 text-white font-medium py-3 px-4 rounded-lg transition-all duration-300 hover:from-purple-800 hover:to-indigo-800 hover:ring-2 hover:ring-white ring-offset-1"
               >
                 AccommoDate
               </button>
