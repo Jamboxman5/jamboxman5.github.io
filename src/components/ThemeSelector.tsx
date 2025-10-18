a56ee85865bdae4de3ecc485c2d11eba6c44ff61
Jamboxman5 <samthompson1818@gmail.com>
2025-10-18 12:00:00 -0400

    Improved theme selector formatting

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -4,21 +4,31 @@ import { useTheme } from "../ThemeContext";
 const ThemeSelector: React.FC = () => {
   const { theme, setTheme, availableThemes } = useTheme();
 
+  const format = (x: String) => {
+    return x.charAt(0).toUpperCase() + x.substring(1);
+  }
+
   return (
-    <div className="space-x-2 w-1/8 py-3 flex mx-auto flex-row items-center justify-center">
-      {availableThemes.map((t) => (
+    <div className="space-x-2 w-1/8 py-3 flex mx-auto flex-row items-center justify-center text-white font-normal">
+      <div>
+        Theme:
+      </div>
+      <div className="flex flex-row space-x-2">
+        {availableThemes.map((t) => (
         <button
           key={t}
           onClick={() => setTheme(t)}
-          className={`px-3 py-1 rounded mx-auto ${
+          className={`px-3 py-1 rounded-lg ml-2 ${
             theme === t
               ? "bg-primary text-white"
               : "bg-gray-200 text-black hover:bg-gray-300"
           }`}
         >
-          {t}
+          {format(t)}
         </button>
       ))}
+      </div>
+      
     </div>
   );
 };
