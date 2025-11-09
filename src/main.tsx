2785a2a1f319bf3550d7335d8cffbf8f3f040337
Samuel Thompson <samthompson1818@gmail.com>
2025-11-09 00:54:52 -0500

    Added scroll reset on page change

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,12 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { ThemeProvider } from "./ThemeContext.tsx";
 import Footer from "./components/Footer.tsx";
-
+import ScrollToTop from "./util/ResetScroll.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <ThemeProvider>
+        <ScrollToTop />
         <App />
         <Footer />
       </ThemeProvider>
